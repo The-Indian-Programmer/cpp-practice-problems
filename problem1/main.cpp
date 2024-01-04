8b9d97d8c308637c133974bead6ed5da6c542d0f
Tim Dev <bresnont@gmail.com>
2024-01-04 19:41:01 +0530

    Problem 1

---
+++ problem1/main.cpp
@@ -0,0 +1,14 @@
+// C++ Program to Get Input from the User
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Enter a number: ";
+    int userInput;
+    cin >> userInput;
+    cout << "You entered: " << userInput << endl;
+
+    return 0;
+}
